4cf71233e8ac07a410a32f328bad9c82bc009846
Kato <kubkate@gmail.com>
2020-09-10 10:57:47 -0700

    create songs list and rename images

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
-// CHECK IF PLAYIING
+
+// ======== SONG LIST ===============
+const songs = [
+  {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto'
+  },
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto'
+  },
+  {
+    name: 'jacinto-3',
+    displayName: 'Goodnight Disco Queen',
+    artist: 'Jacinto'
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto'
+  }
+];
+
+
+// ======== CONTROLS ================
+// Check if playing
 let isPlaying = false;
 
 // PLAY
